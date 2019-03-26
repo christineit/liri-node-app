4a9d0d9fa99f83da8ce9e4af23d6c5e18c910813
christineit <christineitrinh@gmail.com>
2019-03-25 19:56:28 -0500

    added packages and command options

--- liri.js
+++ liri.js
@@ -1,8 +1,31 @@
-require("dotenv").config();
-
-var keys = require("./keys.js");
+//Node modules needed to run the functions:
+require("dotenv").config(); //loads environment variables from .env
+var keys = require("./keys.js"); //loads keys to app
+var fs = require("fs"); // read and writes files
+var axios = require("axios"); //performs API requests to backend
+var Spotify = require("node-spotify-api"); // API library for Spotify REST API
 
 var spotify = new Spotify(keys.spotify);
+
+//2nd index position for action command in terminal
+var action = process.argv[2];
+//Commands for liri app:
+switch (action) {
+  case "concert-this":
+    concertThis();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("liri node app");
+}
 
 /*concert-this
 node liri.js concert-this <artist/band name here>
@@ -10,7 +33,10 @@ Outputs:
 Name of the venue
 Venue location
 Date of the Event (use moment to format this as "MM/DD/YYYY")
+*/
+function concertThis() {}
 
+/*
 spotify-this-song
 node liri.js spotify-this-song '<song name here>'
 Outputs:
@@ -21,7 +47,9 @@ The album that the song is from
 If no song is provided then your program will default to "The Sign" by Ace of Base.
 npm init -y
 npm i node-spotify-api
+*/
 
+/*
 movie-this
 node liri.js movie-this '<movie name here>'
 Outputs: 
@@ -36,7 +64,9 @@ Outputs:
 If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 npm init -y
 npm i axios
+*/
 
+/*
 do-what-it-says
 npm init -y
 npm i npm i file-system
